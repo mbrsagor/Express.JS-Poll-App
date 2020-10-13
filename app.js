c51b718ffc77d91898948b76cb25adef98bde2b3
Sagor <mbrsagor@gmail.com>
2020-10-14 02:10:10 +0600

    Create new route pasing json

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 
 // Create route
+
+app.get('/json', (req, res) => {
+    let _data = {name: 'mbr-sagor', age:25, phone:'+8801773474709', address:'Dhaka-Bangladesh'}
+    res.json(_data);
+})
+
 app.get('/', (req, res) => {
     res.send("Hello, I'm homepage");
 });
@@ -16,5 +22,5 @@ app.get('*', (req, res) => {
 // Server config
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server is started localhost:${PORT}`);
+    console.log(`Starting development server at http://localhost:${PORT}\nSystem check identified no issues\nQuit the development server with CONTROL-C.`);
 })
